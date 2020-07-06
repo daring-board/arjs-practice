d97b904638a41bf7f85ac53ab48f6ccba78a9910
Mahoto Iwasa <mahotox101500@gmail.com>
2020-07-06 15:58:19 +0900

    modify

--- index.js
+++ index.js
@@ -27,17 +27,18 @@ AFRAME.registerComponent('change-color-on-hover', {
 
       el.addEventListener('mouseenter', function () {
         el.setAttribute('color', data.color);
-        var cursor = document.querySelector('a-camera');
-        var pos = cursor.getAttribute('position');
-        var rot = cursor.getAttribute('rotation');
-        cursor.setAttribute('position', '0 2 3');
+        var camera = document.querySelector('a-camera');
+        var pos = camera.getAttribute('position');
+        var rot = camera.getAttribute('rotation');
+        camera.setAttribute('position', '0 2 3');
         console.log(pos);
         console.log(rot);
       });
 
       el.addEventListener('mouseleave', function () {
         el.setAttribute('color', defaultColor);
-        cursor.setAttribute('position', '0 1.6 -1');
+        var camera = document.querySelector('a-camera');
+        camera.setAttribute('position', '0 1.6 -1');
       });
     }
 });
