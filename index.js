ac39403e8857ff6955e40f169e17ed277d603555
Mahoto Iwasa <mahotox101500@gmail.com>
2020-07-07 14:31:37 +0900

    modify

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ async function main(video){
             var camera = document.getElementById('myCamera');
             var rotate = camera.getAttribute('rotation');
             console.log(rotate);
-            const ctr_x = 0.75;
+            const ctr_x = -0.75;
             const ctr_y = 1.6;
             var x = 2 * (nose.position.x / w + ctr_x);
             var y = 2 * (- nose.position.y / h + ctr_y);
