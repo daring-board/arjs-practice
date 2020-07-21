cdf2cd5dccb24aa777e1f9684e475df852c88e04
Mahoto Iwasa <mahotox101500@gmail.com>
2020-07-21 16:55:59 +0900

    modify

--- index.js
+++ index.js
@@ -38,25 +38,21 @@ AFRAME.registerComponent('change-color-on-hover', {
 async function main(video){
     const w = 300;
     const h = 400;
-    const model = await handTrack.load({
-        flipHorizontal: false,   // flip e.g for video 
-        imageScaleFactor: 0.7,  // reduce input image size for gains in speed.
-        maxNumBoxes: 1,        // maximum number of boxes to detect
-        iouThreshold: 0.75,      // ioU threshold for non-max suppression
-        scoreThreshold: 0.75,    // confidence threshold for predictions.
-    });
+    const model = await facemesh.load();
     var sphere = document.getElementById('sphere');
     while(true) {
-        let hands = await model.detect(video);
+        let faces = await model.estimateFaces(video);
 
         const camera = document.getElementById('myCamera');
         const rotate = camera.getAttribute('rotation');
         const radius = 2;
         const radian = rotate.y / 180 * Math.PI;
+        console.log(faces)
 
-        if (hands.length > 0) {
-            const x = hands[0].bbox[0] + hands[0].bbox[2] / 2
-            const y = hands[0].bbox[1] + hands[0].bbox[3] / 2
+        if (faces.length > 0) {
+            const keypoints = prediction.scaledMesh;
+            const x = keypoints[0][0]
+            const y = keypoints[0][1]
             var position = { x: 0, y: 1, z: 0};
             position.x = - Math.sin(radian) - ((x - w/2 - w/5)/w) * 2 * Math.cos(radian);
             position.y += 4 * ((-y + h/2)/h);
